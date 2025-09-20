3de7fd4061d65ddc3ff01490ea937be74513cfa5
Joset Santiago <josetsinph@gmail.com>
2025-09-20 20:34:01 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let who = ['mi café','mi gato','mi despertador','mi cama'];
+let action = ['no estaba listo','estaba muy apapachable','no sonó','estaba muy suave'];
+let what = ['y no pude beber','y tuve que acariciarlo','y no me desperté','y me atrapó'];
+let when = ['esta mañana.','toda la mañana.','a la hora.','todo el fin de semana incluyendo hoy lunes.'];
+
+let container = document.getElementById('excuse');
+container.innerHTML = ''; 
+
+let i = Math.floor(Math.random() * who.length); // prueba de índice aleatorio
+
+let excuse = who[i] + ' ' + action[i] + ' ' + what[i] + ' ' + when[i];
+
+let p = document.createElement('p');
+p.textContent = excuse;
+container.appendChild(p);
+
+console.log("Frase aleatoria mostrada:", excuse);
 };
